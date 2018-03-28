f3ca308b93300786efc636b574c2f68df7d9701f
Ryan Walter <rwalter215@gmail.com>
2018-03-28 17:06:24 -0500

    removed cra app, added bootstrap react and create-component-app

---
+++ src/containers/TopNav/TopNav.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./TopNav.css";
+
+class TopNav extends Component {
+  state = {};
+
+  render() {
+    return <div className="TopNav">TopNav</div>;
+  }
+}
+
+TopNav.propTypes = {};
+
+TopNav.defaultProps = {};
+
+export default TopNav;
---
+++ templates/class/COMPONENT_NAME.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./COMPONENT_NAME.css";
+
+class COMPONENT_NAME extends Component {
+  state = {};
+
+  render() {
+    return <div className="COMPONENT_NAME">COMPONENT_NAME</div>;
+  }
+}
+
+COMPONENT_NAME.propTypes = {};
+
+COMPONENT_NAME.defaultProps = {};
+
+export default COMPONENT_NAME;
