46197039a5284186b5a710358cbb9cf0c0a4876d
Jack Hall <jackjhall22@me.com>
2018-03-29 21:24:58 -0700

    initial

--- src/containers/TopNav/TopNav.js
+++ src/containers/TopNav/TopNav.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import "./TopNav.css";
 import NavItem from "components/NavItem/NavItem";
 import { Row } from "react-bootstrap";
